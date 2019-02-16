321b515046eaaf071e8d755de14b0dd1f63f67f8
oormilaghantasala <oormilaghantasala@gmail.com>
2019-02-16 18:02:46 +0530

    Added API to get Response, UI for Conversation, other minor UI changes

--- chatbot_app/views.py
+++ chatbot_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-
 def index(request):
 	context = {}    
 	return render(request, 'index.html', context=context)
+	
+@csrf_exempt
+def get_reply(request):
+	response = {"reply": "Hi"}
+	return JsonResponse(response)
