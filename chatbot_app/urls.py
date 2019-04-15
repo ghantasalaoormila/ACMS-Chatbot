ee03cd50ba56c0ca80dbf9e58f839855c41209a7
oormilaghantasala <oormilaghantasala@gmail.com>
2019-04-15 15:10:11 +0530

    Removed duplicate files

--- chatbot_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-	path('', views.index, name='index'),
-	url(r'^get_reply$', views.get_reply, name='get_reply'),   
-]
